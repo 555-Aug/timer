cfd4fb837f9a41d26997b6079da5a6147f3f86fe
555aug <cloha.shiroha@gmail.com>
2019-02-19 23:54:47 +0900

    初期画面を作成。テストはあきらめた

---
+++ app/src/test/java/com/example/myapplication/MainActivityTest.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    /**
+     * 03:00が設定されている
+     */
+    @Test
+    fun showInitialText() {
+        // TODO impl
+    }
+}
